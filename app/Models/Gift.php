8f83864f54ee0ea228d9a6e403c650d97a875602
fanarkt08 <fanarkt08@gmail.com>
2026-04-16 10:43:04 +0200

    update activity 8 - verified

--- app/Models/Gift.php
+++ app/Models/Gift.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Gift extends Model
 {
-    protected $fillable = ['name', 'url', 'details', 'price'];
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'url',
+        'details',
+        'price'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+    ];
 }
